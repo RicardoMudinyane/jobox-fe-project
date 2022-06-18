8089185d9ca1908e05c7705ea0a197ec8da6c5e9
RicardoMudinyane <ricardomudinyane@gmail.com>
2022-06-18 23:42:04 +0200

    Open and Close form

---
+++ index.js
@@ -0,0 +1,8 @@
+function openForm() {
+  document.getElementById("myForm").style.display = "block";
+  document.getElementById("addBtn").style.display = "none";
+}
+function closeForm() {
+  document.getElementById("myForm").style.display = "none";
+  document.getElementById("addBtn").style.display = "block";
+}
